20314a4b44d9a8c0302f7401916338b326e1613a
muhibk22 <m.muhibk22@gmail.com>
2025-12-30 23:15:46 +0500

    refactor: reorganize code and improve modularity

--- apps/web/src/components/admin/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AdminSidebar } from './Sidebar';
-export { default as DashboardLayout } from './DashboardLayout';
-export { default as StatsCard } from './StatsCard';
-export { default as DataTable } from './DataTable';
